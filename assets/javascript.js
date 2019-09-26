d00f5332e4d567d8731d9ab4919e8101e83fe7dc
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-09-26 10:26:02 +1000

    card design locked in

---
+++ assets/javascript.js
@@ -0,0 +1,15 @@
+$(".cardIcon").hover(
+    function(){
+        $(this).animate({
+            marginTop: "10%",
+        },200);
+        
+    },
+
+    function(){
+        $(this).animate({
+            marginTop: "-0%"
+        },200);
+    }
+)
+
