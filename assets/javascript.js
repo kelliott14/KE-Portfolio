6e246e00c8b43568a4bb2e411ddb48f92ba52656
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-11-19 14:36:14 +1100

    playing around with design

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
+    $(".infoText").hide();
+    $(".infoText").attr("visible-status", "hide");
+
     if ("ontouchstart" in document.documentElement){
         $(".cardInfo").addClass("touchScreen")
     }
 
+    $(".info").on("click", function(){
+        if ($(".infoText").attr("visible-status") == "show"){
+            $(".infoText").hide();
+            $(".infoText").attr("visible-status", "hide");
+        }else {
+            $(".infoText").show();
+            $(".infoText").attr("visible-status", "show");
+        }
+    })
 
 });
