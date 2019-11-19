b84d54e5e9d3e5e6a7911faa6b7f97c84e61095c
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-11-19 16:13:29 +1100

    bio added

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
-    $(".infoText").show("slow", function(){});
-    $(".infoText").attr("visible-status", "show");
+    $(".infoText").hide("slow", function(){});
+    $(".infoText").attr("visible-status", "hide");
+    $(".collapseIcon").css({"transform": "rotate(0deg)"});
 
     if ("ontouchstart" in document.documentElement){
         $(".cardInfo").addClass("touchScreen")
     }
 
-    $(".info").on("click", function(){
+    $(".collapseIcon").on("click", function(){
         if ($(".infoText").attr("visible-status") == "show"){
             $(".infoText").hide("slow", function(){});
             $(".infoText").attr("visible-status", "hide");
+            $(".collapseIcon").css({"transform": "rotate(0deg)"});
         }else {
             $(".infoText").show("slow", function(){});
             $(".infoText").attr("visible-status", "show");
+            $(".collapseIcon").css({"transform": "rotate(-180deg)"});
         }
     })
 
