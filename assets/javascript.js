0f6d0c1f800498c3d6c71848fd6cc8c5b78b4a39
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-11-22 10:20:22 +1100

    changed card colours to jquery

--- assets/javascript.js
+++ assets/javascript.js
@@ -20,15 +20,32 @@ $(document).ready(function() {
     });
 
     var num;
+    var scheme = {
+        1: {
+            background: "#B1504E",
+            font: "#333333"
+        },
+        2: {
+            background: "#80CAD9",
+            font: "#EAE8E4"
+        },
+        3: {
+            background: "#D980A4",
+            font: "#EAE8E4"
+        },
+        4: {
+            background: "#123840",
+            font: "#EAE8E4"
+        }
+    }
 
     $(".cardInfo").hover( function() {
         num = Math.floor(Math.random() * 4) + 1;
-        $(this).addClass("cardOpt" + num)
-    },
-    
-        function() {
-            $(this).removeClass("cardOpt" + num);
-        })
+        $(this).css("background-color", scheme[num].background);
+        $(this).css("color", scheme[num].font);
+        $(this).children(".btn").css("color", scheme[num].font);
+        $(this).children(".btn").css("border-color", scheme[num].font);
+    });
 
 });
 
